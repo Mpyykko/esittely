6c6d143dccb405fd8b491a4619df54b7ef5e88aa
Mikko Pyykkönen <pyykkonenmikko@gmail.com>
2024-06-09 11:49:29 +0300

    mobiilinavia

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -2,7 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     const button = document.querySelector('.mobilenav_button');
     const content = document.querySelector('.mobilenav_content');
 
-    button.addEventListener('click', function() {
+    button.addEventListener('click', function(event) {
+        event.stopPropagation();
         content.classList.toggle('open');
+    });
+
+    document.addEventListener('click', function(event) {
+        const targetElement = event.target;
+        const isInsideMobileNav = content.contains(targetElement);
+
+        if (!isInsideMobileNav) {
+            content.classList.remove('open');
+        }
     });
 });
